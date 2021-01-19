a168e49ce51b00eef8ae130651718ac78183e3db
unknown <tru3.prodigy@gmail.com>
2021-01-19 12:51:41 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import Express from 'express';
-
-
 const app = Express()
+
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+    ]
+    app.get('/users', function(req,res){
+         res.json({
+              success: true,
+              message: 'successfully got users. Nice!',
+              users: mockUserData
+         })
+    })
 
 app.listen(8000, () => {
     console.log("Server is running")
